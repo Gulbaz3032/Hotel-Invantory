b7335d4855836829db56a6494e4c7a31d2e272ba
Gulbaz3032 <gulbazkachoo3032@gmail.com>
2026-01-16 21:24:22 +0500

    daily reports controller code is done

--- src/controllers/dailyReportController.ts
+++ src/controllers/dailyReportController.ts
@@ -1,6 +1,49 @@
-import type { Request } from "express";
+import type { Request, Response } from "express";
+import { StockTransaction } from "../models/stockTransactionModel.js";
 
 export const getDailyReports = async (req: Request, res: Response) => {
     const { date } = req.query;
     
+    const selectedDate = new Date(date as string);
+    const nextDate = new Date(selectedDate);
+    nextDate.setDate(nextDate.getDate() + 1);
+
+    const report = await StockTransaction.aggregate([
+        {
+            $match: {
+                type: "OUT",
+                createdAt: {
+                    $gte: selectedDate,
+                    $lt: nextDate
+                }
+            }
+        },
+        {
+            $group: {
+                _id: "$item",
+                totalUsed: { $sum : "$quantity" }
+            }
+        },
+        {
+            $lookup: {
+                from: "items",
+                localField: "_id",
+                foreignField: "_id",
+                as: "item"
+            }
+        },
+        { $unwind: "$item" },
+         {
+      $project: {
+        _id: 0,
+        itemName: "$item.name",
+        unit: "$item.unit",
+        totalUsed: 1
+      }
+    }
+    ]);
+    res.status(200).json({
+        message: "Successfully s",
+        report: report
+    })
 }
